a75867d4075a218bd1d6733834690c4133738fdf
J <271398741@qq.com>
2026-01-28 23:43:40 +0800

    add value for diff

--- Sources/MarketKit/Classes/Providers/CoinPriceSchedulerProvider.swift
+++ Sources/MarketKit/Classes/Providers/CoinPriceSchedulerProvider.swift
@@ -48,7 +48,7 @@ extension CoinPriceSchedulerProvider: ISchedulerProvider {
         var coinPrices = try await provider.coinPrices(coinUids: coinUids, walletCoinUids: walletCoinUids, currencyCode: currencyCode)
         // add src20CoinPrices
         let src20Prices = try await provider.safeAllSrc20CoinPrice()
-        let src20CoinPrices = src20Prices.map { CoinPrice(coinUid: TokenQuery(blockchainType: .safe4, tokenType: .eip20(address: $0.address)).customCoinUid, currencyCode: "USD", value: Decimal(string: $0.price) ?? 0 , diff24h: nil, diff1d: nil, timestamp: Date().timeIntervalSince1970) }
+        let src20CoinPrices = src20Prices.map { CoinPrice(coinUid: TokenQuery(blockchainType: .safe4, tokenType: .eip20(address: $0.address)).customCoinUid, currencyCode: "USD", value: Decimal(string: $0.price) ?? 0 , diff24h: Decimal(string: $0.change), diff1d: Decimal(string: $0.change), timestamp: Date().timeIntervalSince1970) }
         coinPrices.append(contentsOf: src20CoinPrices)
         // add safe4 CoinPrice
         if let price = coinPrices.filter({ $0.coinUid.isSafeCoin }).first {
